3554d3d0dfa610304ceeee57a7d6c01bf945d76f
FirdausYNWA <firdausynwa5@gmail.com>
2023-11-20 11:56:09 +0700

    menu finih

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ toggle.addEventListener("click", () => {
   sidebar.classList.toggle("close");
 });
 
+searchBtn.addEventListener("click", () => {
+  sidebar.classList.remove("close");
+});
+
 modeSwich.addEventListener("click", () => {
   body.classList.toggle("dark");
 
